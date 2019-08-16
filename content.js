8263d6ea7e33e15205a0e2d40f5246dd80f0a054
yancymin <senseslofter@gmail.com>
2019-08-16 17:36:51 +0800

    -

--- content.js
+++ content.js
@@ -1,9 +1,7 @@
-/**
- * judement dark mode
- */
+/**  judement dark mode  **/
 
 function css() {
-  var cssId = 'myCss' // you could encode the css path itself to generate id..
+  var cssId = 'myCss'
   if (!document.getElementById(cssId)) {
     var head = document.getElementsByTagName('head')[0]
     var link = document.createElement('link')
@@ -14,7 +12,7 @@ function css() {
     link.media = 'all'
     head.appendChild(link)
   }
-  var cssId = 'myCss2' // you could encode the css path itself to generate id..
+  var cssId = 'myCss2' 
   if (!document.getElementById(cssId)) {
     var head = document.getElementsByTagName('head')[0]
     var link = document.createElement('link')
