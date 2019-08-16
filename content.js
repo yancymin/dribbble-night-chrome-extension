679427274577b717b57f06317c2c721568676023
yancymin <senseslofter@gmail.com>
2019-08-16 16:24:19 +0800

    localstorage

--- content.js
+++ content.js
@@ -1,29 +1,6 @@
-let switchNode = document.createElement('li')
-let tabs = document
-  .getElementById('new-header')
-  .getElementsByClassName('tabs')[2]
-
-switchNode.innerHTML = `
-<span class="menu-non-link">
-               Dark Mode
-                <div class="availability-toggle">
-                  <div class="availablity-toggle-bar"></div>
-                  <a class="availability-toggle-target" data-remote="true" rel="nofollow" data-method="put">
-                      <svg viewBox="0 0 24 24" width="24" height="24" role="img" aria-labelledby="4ohehg8azdfp612f3i6o4fiasx4fwxx" class="icon "><title id="4ohehg8azdfp612f3i6o4fiasx4fwxx">padlock</title>
-<path d="M20 8h-2V6c0-3.309-2.691-6-6-6S6 2.691 6 6v2H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2zm-8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm4-11H8V6c0-2.206 1.794-4 4-4s4 1.794 4 4z"></path></svg>
-
-</a>                </div>
-                <span class="menu-availability-info" rel="tipsy" data-tipsy-class="tipsy-alt" original-title="You are publicly available for hire">
-                </span>
-              </span>
-`
-
-switchNode.id = 'switchMode'
-tabs.appendChild(switchNode)
-
-let switchBtn = switchNode.getElementsByTagName('a')
-let open = switchNode.getElementsByClassName('availability-toggle')[0]
-let dot = switchNode.getElementsByClassName('availability-toggle-target')[0]
+/**
+ * judement dark mode
+ */
 
 function css() {
   var cssId = 'myCss' // you could encode the css path itself to generate id..
@@ -57,14 +34,60 @@ function removeCss() {
   removed === self
 }
 
-switchNode.addEventListener('click', function() {
-  if (open.className == 'availability-toggle') {
-    open.classList.add('publicly')
+chrome.storage.local.get(['dribbbleDarkMode'], function(status) {
+  if (status.dribbbleDarkMode) {
     css()
-    console.log(open.className)
   } else {
-    console.log('remove')
     removeCss()
-    open.classList.remove('publicly')
   }
 })
+
+document.onreadystatechange = function() {
+  if (document.readyState === 'interactive') {
+    let switchNode = document.createElement('li')
+    let tabs = document
+      .getElementById('new-header')
+      .getElementsByClassName('tabs')[2]
+
+    switchNode.innerHTML = `
+        <span class="menu-non-link">
+                    Dark Mode
+                        <div class="availability-toggle">
+                        <div class="availablity-toggle-bar"></div>
+                        <a class="availability-toggle-target" data-remote="true" rel="nofollow" data-method="put">
+                            <svg viewBox="0 0 24 24" width="24" height="24" role="img" aria-labelledby="4ohehg8azdfp612f3i6o4fiasx4fwxx" class="icon "><title id="4ohehg8azdfp612f3i6o4fiasx4fwxx">padlock</title>
+        <path d="M20 8h-2V6c0-3.309-2.691-6-6-6S6 2.691 6 6v2H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2zm-8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm4-11H8V6c0-2.206 1.794-4 4-4s4 1.794 4 4z"></path></svg>
+
+        </a>                </div>
+                        <span class="menu-availability-info" rel="tipsy" data-tipsy-class="tipsy-alt" original-title="You are publicly available for hire">
+                        </span>
+                    </span>
+        `
+
+    switchNode.id = 'switchMode'
+    tabs.appendChild(switchNode)
+
+    let open = switchNode.getElementsByClassName('availability-toggle')[0]
+    let dot = switchNode.getElementsByClassName('availability-toggle-target')[0]
+
+    chrome.storage.local.get(['dribbbleDarkMode'], function(status) {
+      if (status.dribbbleDarkMode) {
+        open.classList.add('publicly')
+      } else {
+        open.classList.remove('publicly')
+      }
+    })
+
+    dot.addEventListener('click', function() {
+      if (open.className == 'availability-toggle') {
+        css()
+        chrome.storage.local.set({ dribbbleDarkMode: true })
+        open.classList.add('publicly')
+      } else {
+        removeCss()
+        chrome.storage.local.set({ dribbbleDarkMode: false })
+        open.classList.remove('publicly')
+      }
+    })
+  }
+}
